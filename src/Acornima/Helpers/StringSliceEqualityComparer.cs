19ae225efa50068d1ec3d01d60a23d6e227f969c
JavaCommons Technologies <javacommons@gmail.com>
2026-01-22 14:27:53 +0900

    .

--- src/Acornima/Helpers/StringSliceEqualityComparer.cs
+++ src/Acornima/Helpers/StringSliceEqualityComparer.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 
-// ReSharper disable once CheckNamespace
 namespace JavascriptParser.Helpers;
 
 internal class StringSliceEqualityComparer : IEqualityComparer<ReadOnlyMemory<char>>
---
+++ update.sh
@@ -0,0 +1,26 @@
+#! /usr/bin/env bash
+#! /usr/bin/env bash
+set -uvx
+set -e
+cd "$(dirname "$0")"
+cwd=`pwd`
+ts=`date "+%Y.%m%d.%H%M.%S"`
+bash --version 2>&1 | head -n 1
+
+rm -rf tmp
+mkdir -p tmp
+cd tmp
+git clone --recursive https://github.com/adams85/acornima
+cd acornima
+echo "Microsoft (R) .NET SDK version $(dotnet --version)"
+dotnet build build/_build.csproj -nodeReuse:false -p:UseSharedCompilation=false -nologo -clp:NoSummary --verbosity quiet
+#dotnet run --project build/_build.csproj --no-build -- "$@"
+find . -name "*.csproj" -exec rm -rf {} +
+find . -name "*.cs" -exec sed -i -e "s/Acornima/JavascriptParser/g" {} +
+#find . -name "*.csproj" -exec sed -i -e "s/>Acornima</>JavascriptParser</g" {} +
+cp -r src/Acornima/* $cwd/src/Acornima/
+cp -r src/Acornima.Extras/* $cwd/src/Acornima/
+
+cd $cwd
+mv src/Acornima/Ast/Node.cs src/Acornima/Ast/Node.cs.orig
+cp -r modified/* .
