588372e4acff844464097d59b2612c50aeb93373
JavaCommons Technologies <javacommons@gmail.com>
2026-01-22 14:33:49 +0900

    .

--- replace.sh
+++
@@ -1,4 +0,0 @@
-#! /usr/bin/env bash
-find . -name "*.cs" -exec sed -i -e "s/Acornima/JavascriptParser/g" {} +
-find . -name "*.csproj" -exec sed -i -e "s/>Acornima</>JavascriptParser</g" {} +
-#find . -name "*.csproj" -exec sed -i -e "s/>Acornima.Extras</>JavascriptParser.Extras</g" {} +
